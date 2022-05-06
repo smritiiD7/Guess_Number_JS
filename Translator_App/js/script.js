ee1347d3d46d02752e257da51a79d26dbc387ea4
SmritiiD7 <smritidubey662gmail.com>
2022-05-06 13:13:27 +0530

    Sending Request to API & Fetching Data of Entered Text From API

--- Translator_App/js/script.js
+++ Translator_App/js/script.js
@@ -1,4 +1,9 @@
+const fromText = document.querySelector(".from-text");
 const selectTag = document.querySelectorAll("select");
+translateBtn = document.querySelector("button");
+
+
+
 
 selectTag.forEach((tag,id) =>{
   for(const country_code in countries){
@@ -14,4 +19,17 @@ else if(id==1 && country_code == 'hi-IN'){
  let option = `<option value="${country_code}"${selected}>${countries[country_code]}</option>`;
  tag.insertAdjacentHTML("beforeend",option)//adding option tag inside select tag
   }
+});
+
+translateBtn.addEventListener("click",()=>{
+let text = fromText.value,
+translateFrom = selectTag[0].value, //getting from select tagvalue
+translateTo = selectTag[1].value;
+//console.log(text, translateFrom, tranlateTo);
+let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`; //api fetch
+//fetching api response and returning it with parsing into js object
+//and in another method rescevingg the object
+fetch(apiUrl).then(res=>res.json()).then(data=>{
+  console.log(data);
 })
+});
